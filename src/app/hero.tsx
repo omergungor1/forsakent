5b9b0f018f0fc249a14d6929347adef3985c0b74
Ömer <omergungorco@gmail.com>
2025-01-26 00:42:38 +0300

    First commit

---
+++ src/app/hero.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { IconButton, Button, Typography } from "@material-tailwind/react";
+import { PlayIcon } from "@heroicons/react/24/solid";
+
+function Hero() {
+  return (
+    <div className="relative min-h-screen w-full bg-[url('/image/image1.jpg')] bg-cover bg-no-repeat">
+      <div className="absolute inset-0 h-full w-full bg-gray-900/60" />
+      <div className="grid min-h-screen px-8">
+        <div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
+          <Typography variant="h3" color="white" className="mb-2">
+            test
+          </Typography>
+          <Typography variant="h1" color="white" className="lg:max-w-3xl">
+            Forsa
+          </Typography>
+          <Typography
+            variant="lead"
+            color="white"
+            className="mt-1 mb-12 w-full md:max-w-full lg:max-w-2xl"
+          >
+            Peyzaj Çözümleri
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
