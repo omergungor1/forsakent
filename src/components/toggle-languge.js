aa77e0041e7678989d863dad31962cb10721ae9e
Ömer <omergungorco@gmail.com>
2025-02-01 14:14:09 +0300

    lang queue changed

--- src/components/toggle-languge.js
+++ src/components/toggle-languge.js
@@ -14,8 +14,8 @@ export default function LanguageToggle() {
         onClick={toggleLanguage}
         className="flex items-center dark:bg-gray-800 rounded-full shadow-md hover:bg-gray-200 dark:hover:bg-gray-700 transition border-2 border-gray-300 hover:border-gray-500">
         <Image
-          src={`/flags/${language === "tr" ? "tr.png" : "gb.png"}`}
-          alt={language === "tr" ? "Türkçe" : "English"}
+          src={`/flags/${language === "tr" ? "gb.png" : "tr.png"}`}
+          alt={language === "tr" ? "English" : "Türkçe"}
           width={24}
           height={24} />
       </button>
